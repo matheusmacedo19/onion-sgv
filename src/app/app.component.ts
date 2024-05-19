c0dfffef0f59c258d04811989a920678dae7a750
MatheusMacedo <matheusbarbosa.m9@gmail.com>
2024-05-19 10:47:19 -0300

    push front project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { NavComponent } from './view/components/templates/nav/nav.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet, MatSlideToggleModule, NavComponent],
+  templateUrl:'app.component.html'
 })
 export class AppComponent {
   title = 'SGV';
